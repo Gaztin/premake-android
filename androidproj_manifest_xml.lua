d390fb539b9339f4d975b82b7a1995ad3a3a82ef
Sebastian Kylander <sebastian@kylander.me>
2018-02-23 23:27:08 +0100

    Fix application name in android manifest

--- androidproj_manifest_xml.lua
+++ androidproj_manifest_xml.lua
@@ -9,10 +9,10 @@ local manifest = android.manifest
 
 function manifest.generate(prj)
 	_p(0, '<?xml version="1.0" encoding="utf-8"?>')
-	_p(0, '<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.$(ApplicationName)" android:versionCode="1" android:versionName="1.0">')
+	_p(0, '<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.%s" android:versionCode="1" android:versionName="1.0">', prj.name)
 	_p(1, '<uses-sdk android:minSdkVersion="9" android:targetSdkVersion="21"/>')
-	_p(1, '<application android:label="' .. prj.name .. '" android:hasCode="false">')
-	_p(2, '<activity android:name="android.app.NativeActivity" android:label="' .. prj.name .. '" android:configChanges="orientation|keyboardHidden">')
+	_p(1, '<application android:label="" android:hasCode="false">', prj.name)
+	_p(2, '<activity android:name="android.app.NativeActivity" android:label="" android:configChanges="orientation|keyboardHidden">', prj.name)
 	_p(3, '<meta-data android:name="android.app.lib_name" android:value="$(AndroidAppLibName)" />')
 	_p(3, '<intent-filter>')
 	_p(4, '<action android:name="android.intent.action.MAIN" />')
