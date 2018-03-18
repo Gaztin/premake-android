39e2d92db49f73ae1a71187a3204715f3080f19d
Sebastian Kylander <sebastian@kylander.me>
2018-03-18 22:27:59 +0100

    Fix app label in generated manifest

--- androidproj_manifest_xml.lua
+++ androidproj_manifest_xml.lua
@@ -9,10 +9,10 @@ local manifest = android.manifest
 
 function manifest.generate(prj)
 	_p(0, '<?xml version="1.0" encoding="utf-8"?>')
-	_p(0, '<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.%s" android:versionCode="1" android:versionName="1.0">', prj.name)
+	_p(0, '<manifest xmlns:android="http://schemas.android.com/apk/res/android" package="com.%s" android:versionCode="1" android:versionName="1.0">', prj.targetname)
 	_p(1, '<uses-sdk android:minSdkVersion="9" android:targetSdkVersion="21"/>')
-	_p(1, '<application android:label="" android:hasCode="false">', prj.name)
-	_p(2, '<activity android:name="android.app.NativeActivity" android:label="" android:configChanges="orientation|keyboardHidden">', prj.name)
+	_p(1, '<application android:label="%s" android:hasCode="false">', prj.targetname)
+	_p(2, '<activity android:name="android.app.NativeActivity" android:label="%s" android:configChanges="orientation|keyboardHidden">', prj.targetname)
 	_p(3, '<meta-data android:name="android.app.lib_name" android:value="$(AndroidAppLibName)" />')
 	_p(3, '<intent-filter>')
 	_p(4, '<action android:name="android.intent.action.MAIN" />')
